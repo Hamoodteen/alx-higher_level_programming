6e5859d2107b5512aaf536a45f67109f7c28a380
Hamoodteen <ahmed104567@outlook.com>
2023-10-03 18:03:20 +0300

    0

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_node - f
+ * @head: list
+ * @number: int
+ * Return: list
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	(void)number;
+	return (*head);
+}
