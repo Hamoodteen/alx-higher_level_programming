92e88b53f5a007f01d5068482cb78320172b5c30
Hamoodteen <ahmed104567@outlook.com>
2023-10-03 05:37:18 +0300

    0

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,59 +1,6 @@
 #include "lists.h"
 
 /**
- * print_listint - f
- * @h: int
- * Return: 0
-*/
-size_t print_listint(const listint_t *h)
-{
-	size_t i;
-
-	for (i = 0; h != NULL; i++)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (i);
-}
-
-/**
- * add_nodeint - f
- * @head: list
- * @n: int
- * Return: 0
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *nn;
-
-	if (head == NULL)
-		return (NULL);
-	nn = malloc(sizeof(listint_t));
-	if (nn == NULL)
-	{
-		free(nn);
-		return (NULL);
-	}
-	nn->n = n;
-	nn->next = *head;
-	*head = nn;
-	return (nn);
-}
-
-/**
- * free_listint - f
- * @head: list
-*/
-void free_listint(listint_t *head)
-{
-	if (head == NULL)
-		return;
-	free_listint(head->next);
-	free(head);
-}
-
-/**
  * check_cycle - f
  * @list: list
  * Return: 0 or 1
@@ -61,5 +8,6 @@ void free_listint(listint_t *head)
 int check_cycle(listint_t *list)
 {
 	int i = list->n;
+
 	return (i);
 }
---
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * print_listint - f
+ * @h: int
+ * Return: 0
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
+
+/**
+ * add_nodeint - f
+ * @head: list
+ * @n: int
+ * Return: 0
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *nn;
+
+	if (head == NULL)
+		return (NULL);
+	nn = malloc(sizeof(listint_t));
+	if (nn == NULL)
+	{
+		free(nn);
+		return (NULL);
+	}
+	nn->n = n;
+	nn->next = *head;
+	*head = nn;
+	return (nn);
+}
+
+/**
+ * free_listint - f
+ * @head: list
+*/
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
