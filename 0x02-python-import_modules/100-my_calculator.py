195ecf5de567c5916e90d97f85d389a5c3f033be
Hamoodteen <ahmed104567@outlook.com>
2023-10-05 19:25:19 +0300

    0

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,15 +5,14 @@ if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    match argv[2]:
-        case "+":
-            print("{:d} + {:d} = {:d}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
-        case "-":
-            print("{:d} - {:d} = {:d}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
-        case "*":
-            print("{:d} * {:d} = {:d}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
-        case "/":
-            print("{:d} / {:d} = {:d}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if argv[2] == "+":
+        print("{:d} + {:d} = {:d}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "-":
+        print("{:d} - {:d} = {:d}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "*":
+        print("{:d} * {:d} = {:d}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "/":
+        print("{:d} / {:d} = {:d}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
