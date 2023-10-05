c749849bbf9c4c4bd56bccb85a13247d99096e0a
Hamoodteen <ahmed104567@outlook.com>
2023-10-05 19:22:39 +0300

    0

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 if __name__ == '__main__':
     if len(argv) != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     match argv[2]:
         case "+":
