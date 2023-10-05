66c04c8ef33b46e79f359eb2848d40f5437ed7a0
Hamoodteen <ahmed104567@outlook.com>
2023-10-05 18:52:23 +0300

    0

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-for str in dir(hidden_4):
-    if str[:2] != "__":
-        print("{:s}".format(str))
+if __name__ == '__main__':
+    for str in dir(hidden_4):
+        if str[:2] != "__":
+            print("{:s}".format(str))
