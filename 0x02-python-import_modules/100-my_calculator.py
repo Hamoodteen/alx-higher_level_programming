fcc623f5c300ded011bb405dac1bfe8fb51abdb6
Hamoodteen <ahmed104567@outlook.com>
2023-10-05 19:29:33 +0300

    0

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(argv[1], argv[3], add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(argv[1], argv[3], sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif argv[2] == "*":
-        print("{:d} * {:d} = {:d}".format(argv[1], argv[3], mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif argv[2] == "/":
-        print("{:d} / {:d} = {:d}".format(argv[1], argv[3], div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
