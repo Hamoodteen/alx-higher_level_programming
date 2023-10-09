034b5a813cfbc6692c522e304822f7789715cafa
Hamoodteen <ahmed104567@outlook.com>
2023-10-09 23:11:26 +0300

    0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
+    if len(matrix) == 0:
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
