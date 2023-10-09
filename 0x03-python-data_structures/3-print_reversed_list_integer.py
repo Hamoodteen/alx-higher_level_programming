ebf6c4ebe17f11e9af30f78c1d83a0158e57c6d3
Hamoodteen <ahmed104567@outlook.com>
2023-10-09 17:45:02 +0300

    0

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list = reversed(my_list)
-    for i in my_list:
+    l = len(my_list)
+    my_list2 = my_list.copy()
+    for c in range(l):
+        my_list2[c] = my_list[l-1-c]
+    for i in my_list2:
         print("{:d}".format(i))
---
+++ 0x03-python-data_structures/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    # for c in range(len(my_list)):
+    #     my_list[c] = my_list[len(my_list)-1-c]
