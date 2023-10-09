4567b3ed36fbf4ddda072cdcabb2f6f2b51b088c
Hamoodteen <ahmed104567@outlook.com>
2023-10-09 23:10:54 +0300

    0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix: return
+    if not matrix:
+        return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=" " if j != 2 else "")
