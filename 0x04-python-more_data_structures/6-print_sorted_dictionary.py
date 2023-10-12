9641f91da73ffe46d2ac643fbd701bdbc4e6c1b4
Hamoodteen <ahmed104567@outlook.com>
2023-10-13 00:34:44 +0300

    0

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sort = a_dictionary.copy()
-    print(sort.items() for i in sorted(sort))
+    for i in sorted(sort):
+        print(i)
