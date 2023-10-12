33cf6d4fef82f293cdc716a842b18d783db26596
Hamoodteen <ahmed104567@outlook.com>
2023-10-13 00:17:57 +0300

    0

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,6 @@ def common_elements(set_1, set_2):
     common = set()
     for i in set_1:
         for j in set_2:
-            if i != j:
-                common.add(i)
+            if i not in set_2 and j not in set_1:
+                common.add(i, j)
     return common
