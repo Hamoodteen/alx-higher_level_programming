439ffeab2dc61b65e2093dd6615c4ace11355989
Hamoodteen <ahmed104567@outlook.com>
2023-10-13 18:26:14 +0300

    0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if a_dictionary is None:
         return None
     i = 0
-    for j in a_dictionary.values():
-        if i < j:
-            i = j
-    return i
+    v = ""
+    for j, k in a_dictionary.items():
+        if i < k:
+            i = k
+            v = j
+    return v
