c4784734c2b788b36121a808e12ffe468e9b2ee4
Hamoodteen <ahmed104567@outlook.com>
2023-10-13 21:13:07 +0300

    0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-	units = {'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9}
-	tens = {'X':10,'XX':20,'XXX':30,'XL':40,'L':50,'LX':60,'LXX':70,'LXXX':80,'XC':90}
-	hundreds = {'C':100,'CC':200,'CCC':300,'CD':400,'D':500,'DC':600,'DCC':700,'DCCC':800,'CM':900}
-	thousands = {'M':1000,'MM':2000,'MMM':3000}
-	if type(roman_string) != str or roman_string is None:
-		return 0
+    units = {'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9}
+    tens = {'X':10,'XX':20,'XXX':30,'XL':40,'L':50,'LX':60,'LXX':70,'LXXX':80,'XC':90}
+    hundreds = {'C':100,'CC':200,'CCC':300,'CD':400,'D':500,'DC':600,'DCC':700,'DCCC':800,'CM':900}
+    thousands = {'M':1000,'MM':2000,'MMM':3000}
+    if type(roman_string) != str or roman_string is None:
+        return 0
  
