db65ca0d9b77a34f141061215b9bcac40e19e0e6
Hamoodteen <ahmed104567@outlook.com>
2023-10-13 18:16:07 +0300

    0

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     b_dictionary = a_dictionary.copy()
-    for key in b_dictionary.items():
-        b_dictionary[key] *= 2
+    for key, value in b_dictionary.items():
+        b_dictionary[key] = value * 2
     return b_dictionary
