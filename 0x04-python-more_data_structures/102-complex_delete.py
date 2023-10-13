915cdace228216ad2f49686b53dd11645c142eae
Hamoodteen <ahmed104567@outlook.com>
2023-10-13 22:15:03 +0300

    0

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    deletion = []
     if value not in a_dictionary:
         return a_dictionary
     for i, j in a_dictionary.items():
         if j == value:
-            del a_dictionary[i]
+            deletion.append(i)
+    for k in deletion:
+        a_dictionary.pop(k)
     return a_dictionary
