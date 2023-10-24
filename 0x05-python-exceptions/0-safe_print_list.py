ced6812bcb9d07b1b1a06532a34f21df7e8b5f35
Hamoodteen <ahmed104567@outlook.com>
2023-10-25 01:36:10 +0300

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         print()
         return x
-    except:
+    except IndexError:
         s = 0
         for i in my_list:
             print(i, end="")
