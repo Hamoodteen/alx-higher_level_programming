5d6f9b6bd984e68add2f65fe0754fff33146939c
Hamoodteen <ahmed104567@outlook.com>
2023-10-25 03:28:53 +0300

    0

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ Declares a square class """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        print() if self.__size == 0 else print(end="")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print(" " * self.__position[0], "#", end="")
+            print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
