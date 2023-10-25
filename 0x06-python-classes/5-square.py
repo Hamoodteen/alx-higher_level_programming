6cbb78fe22e6df7e4c9d1646bf679868b800e6b3
Hamoodteen <ahmed104567@outlook.com>
2023-10-25 03:17:10 +0300

    0

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ Declares a square class """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        print() if self.__size == 0 else print(end="")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
