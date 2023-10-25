cb447e3ad9f372752f1814852d10dc6607d5d254
Hamoodteen <ahmed104567@outlook.com>
2023-10-25 03:09:20 +0300

    0

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,19 +5,18 @@
 class Square:
     """ Declares a square class """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def size(self):
         return self.__size
 
     def size(self, value):
-        size = value
-        return size
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size ** 2
