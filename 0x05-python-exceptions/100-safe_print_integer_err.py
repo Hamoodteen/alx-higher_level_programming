807637aca67a1c8a2b447e20461932f6091b644e
Hamoodteen <ahmed104567@outlook.com>
2023-10-25 05:39:03 +0300

    0

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        sys.stderr.write(f"Exception: {e}\n")
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
