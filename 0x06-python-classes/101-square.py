8f9d1ccfea76275cd0ca6ebd2dd674d4427aac8a
Hamoodteen <ahmed104567@outlook.com>
2023-10-25 04:51:48 +0300

    0

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -55,6 +55,4 @@ class Square:
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
         return ("")
