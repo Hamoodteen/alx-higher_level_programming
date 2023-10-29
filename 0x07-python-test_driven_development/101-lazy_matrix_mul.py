16d7b3af1288506dba12d638b50f64fb7f47e091
Hamoodteen <ahmed104567@outlook.com>
2023-10-29 02:49:21 +0200

    0

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+commentttttttttttttttttttttttttttttt
+"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ commenttttttttttttttttttttttttttttt """
+    return np.dot(m_a, m_b)
