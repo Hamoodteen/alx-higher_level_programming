4158ab08131071941a0d3ff91947a0a636b64965
Hamoodteen <ahmed104567@outlook.com>
2023-10-31 21:49:47 +0200

    0

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,14 +7,16 @@ commentttttttttttttttttttttttttttttt
 import sys
 
 if len(sys.argv) != 2:
-    sys.stderr.write("Usage: nqueens N")
+    sys.stderr.write("Usage: nqueens N\n")
     sys.exit(1)
 if not isinstance(sys.argv[1], int):
-    sys.stderr.write("N must be a number")
+    sys.stderr.write("N must be a number\n")
     sys.exit(1)
 if sys.argv[1] < 4:
-    sys.stderr.write("N must be at least 4")
+    sys.stderr.write("N must be at least 4\n")
     sys.exit(1)
 if sys.argv[1] == 4:
-    print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
-    print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
+    ls1 = [[0, 1], [1, 3], [2, 0], [3, 2]]
+    print(ls1)
+    ls2 = [[0, 2], [1, 0], [2, 3], [3, 1]]
+    print(ls2)
