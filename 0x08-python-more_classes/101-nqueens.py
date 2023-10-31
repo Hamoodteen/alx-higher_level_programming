29fffb3dd76e0b6e654eea6cb9ce29bdfab2186b
Hamoodteen <ahmed104567@outlook.com>
2023-10-31 21:59:00 +0200

    0

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,15 +7,15 @@ commentttttttttttttttttttttttttttttt
 from sys import argv, stderr
 
 if len(argv) != 2:
-    print("Usage: nqueens N\n")
+    print("Usage: nqueens N")
     exit(1)
 try:
     na = int(argv[1])
 except ValueError:
-    print("N must be a number\n")
+    print("N must be a number")
     exit(1)
 if na < 4:
-    print("N must be at least 4\n")
+    print("N must be at least 4")
     exit(1)
 if na == 4:
     ls1 = [[0, 1], [1, 3], [2, 0], [3, 2]]
