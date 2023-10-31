922cf5c423a36b6590d1a8da7d4b7227d1de81f1
Hamoodteen <ahmed104567@outlook.com>
2023-10-31 20:31:50 +0200

    0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,7 +38,7 @@ class Rectangle():
     def area(self):
         return self.__width * self.__height
 
-    def permiter(self):
+    def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
