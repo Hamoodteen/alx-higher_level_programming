2c5722dd05d6cb3170964fec3e0d2c195f2f4d29
Hamoodteen <ahmed104567@outlook.com>
2023-10-31 21:29:10 +0200

    0

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle():
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, RecursionError):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if (rect_1.width * rect_1.height) >= (rect_2.height * rect_2.height):
             return rect_1
