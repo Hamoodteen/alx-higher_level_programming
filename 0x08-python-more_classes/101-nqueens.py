73220883e749f7d633077eb1751eb251d6281bdd
Hamoodteen <ahmed104567@outlook.com>
2023-10-31 22:01:42 +0200

    0

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,14 +9,18 @@ from sys import argv
 if len(argv) is not 2:
     print('Usage: nqueens N')
     exit(1)
+
 if not argv[1].isdigit():
     print('N must be a number')
     exit(1)
-na = int(argv[1])
-if na < 4:
+
+N = int(argv[1])
+
+if N < 4:
     print('N must be at least 4')
     exit(1)
-if na == 4:
+
+if N == 4:
     ls1 = [[0, 1], [1, 3], [2, 0], [3, 2]]
     print(ls1)
     ls2 = [[0, 2], [1, 0], [2, 3], [3, 1]]
