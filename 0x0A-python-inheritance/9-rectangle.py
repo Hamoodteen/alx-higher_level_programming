27ecceca9914eadd77a3cf6b6d1bdf1770808e06
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 00:38:44 +0200

    0

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttt"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """commentttttttttttttttttttttttttttt"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self, width, height):
+        return self.__width * self.__height
