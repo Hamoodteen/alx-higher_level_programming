f6fd20701fd10a67940f5e56f9d3cbe72dc3b178
Hamoodteen <ahmed104567@outlook.com>
2023-11-06 23:59:34 +0200

    0

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttt"""
+
+
+def is_kind_of_class(obj, a_class):
+    """commentttttttttttttttttttttttttttt"""
+    return isinstance(obj, a_class)
