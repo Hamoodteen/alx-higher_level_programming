b117d5b6f1906e0afa915a2b0816460a8fa5bf0c
Hamoodteen <ahmed104567@outlook.com>
2023-11-06 23:46:35 +0200

    0

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttt"""
+
+
+class MyList(list):
+    """commentttttttttttttttttttttttttttt"""
+    def print_sorted(self):
+        print(sorted(self))
