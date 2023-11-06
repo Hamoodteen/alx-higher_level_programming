74df25679c18e440b116dd0b962c27f24ddf59da
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 00:57:32 +0200

    0

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttt"""
+
+
+class MyInt(int):
+    """commentttttttttttttttttttttttttttt"""
+    def __eq__(self, other):
+        int.__ne__(other)
+
+    def __ne__(self, other):
+        int.__eq__(other)
