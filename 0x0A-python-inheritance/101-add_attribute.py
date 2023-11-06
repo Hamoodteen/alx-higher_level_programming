b33b9e2976a8f9fe325bab0cb348133b31d23af1
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 01:12:45 +0200

    0

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,5 @@
 def add_attribute(op, n, v):
     """commentttttttttttttttttttttttttttt"""
     if not hasattr(op, n):
-        setattr(op, n, v)
-    else:
-        raise TypeError("can't add new attribute")
+        if not setattr(op, n, v):
+            raise TypeError("can't add new attribute")
