45553eca3d881d703d737f3ebbbb47dd1a645daf
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 00:07:15 +0200

    0

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """commentttttttttttttttttttttttttttt"""
-    return issubclass(obj, a_class) and isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
