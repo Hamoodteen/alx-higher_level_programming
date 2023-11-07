1ea184ebcfbbc2e117358d04c9a718133cd28fcd
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:13:43 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """commentttttttttttttttttttttttttttt"""
-    with open(filename, "r", encoding="UTF-8") as f:
+    with open(filename, encoding="UTF-8") as f:
         f.read()
