1fc66938479a392e25b398a1a27036831f779f06
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:48:24 +0200

    0

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttt"""
+
+
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+"""commentttttttttttttttttttttttttttt"""
+ls = []
+for i in sys.argv:
+    ls.append(i)
+save_to_json_file(ls, "add_item.json")
