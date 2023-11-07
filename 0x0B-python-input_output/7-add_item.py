8f80847c244b5c34cf77b086eb151bb9ced34772
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:52:53 +0200

    0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 """commentttttttttttttttttttttttttttt"""
 ls = load_from_json_file("add_item.json")
+"""commentttttttttttttttttttttttttttt"""
 for i in sys.argv[1:]:
     ls.append(i)
 save_to_json_file(ls, "add_item.json")
