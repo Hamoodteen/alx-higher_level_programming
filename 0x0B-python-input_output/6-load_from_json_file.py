a5830361e6afc561a6d822cf8b2d0119d7502a9d
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:38:45 +0200

    0

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """commentttttttttttttttttttttttttttt"""
-    with open(filename, "w", encoding="UTF-8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         json.load(f)
