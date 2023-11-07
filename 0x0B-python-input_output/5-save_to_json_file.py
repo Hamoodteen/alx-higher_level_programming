a200be49e90cff5cbaa0bdc5f99d4acd5d9dd22f
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:36:31 +0200

    0

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """commentttttttttttttttttttttttttttt"""
-    with open(filename, "w", encode="UTF-8") as f:
+    with open(filename, "w", encoding="UTF-8") as f:
         json.dump(my_obj, f)
