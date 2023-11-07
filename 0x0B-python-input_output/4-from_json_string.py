9bf0f56e65cd5e2697e018b3eb183ee48bd00162
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:31:13 +0200

    0

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttt"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """commentttttttttttttttttttttttttttt"""
+    return json.loads(my_str)
