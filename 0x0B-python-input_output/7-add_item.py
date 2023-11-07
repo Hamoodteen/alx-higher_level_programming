1d33ea804b813771346f65a42f2e2cd1d2531f6b
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:56:51 +0200

    0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,13 +7,11 @@ if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_json_file = \
         __import__("6-load_from_json_file").load_from_json_file
-
-
-"""commentttttttttttttttttttttttttttt"""
-try:
-    ls = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    ls = []
-for i in sys.argv[1:]:
-    ls.append(i)
-save_to_json_file(ls, "add_item.json")
+    """commentttttttttttttttttttttttttttt"""
+    try:
+        ls = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        ls = []
+    for i in sys.argv[1:]:
+        ls.append(i)
+    save_to_json_file(ls, "add_item.json")
