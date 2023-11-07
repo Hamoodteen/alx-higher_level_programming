19a1ae1f5f18c0da0c851b83b1db12415ec9ffcc
Hamoodteen <ahmed104567@outlook.com>
2023-11-07 11:20:33 +0200

    0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     """commentttttttttttttttttttttttttttt"""
     try:
-        with open(filename, "r", encoding="UTF-8") as f:
+        with open(filename, "w", encoding="UTF-8") as f:
             f.write(text)
     except PermissionError:
         print("Permission denied")
