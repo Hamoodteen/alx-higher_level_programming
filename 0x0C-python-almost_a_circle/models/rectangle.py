da046196e19a07e1823e1aad56056cd883ca435c
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 22:54:57 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,8 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """commenttttttttttttttttttttttttttttttt"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
