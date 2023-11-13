ddb101b666b9ef8e3d12019a465fc7b9a15aa532
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 23:37:45 +0200

    0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,16 +7,16 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """commenttttttttttttttttttttttttttttttt"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
+        self.__size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.size)
+            self.id, self.x, self.y, self.__size)
 
     @property
     def size(self):
-        return self.width
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -24,5 +24,5 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
