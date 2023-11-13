790b06e3131edb8aca9a77a74bd1999652390e82
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 23:45:08 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,4 +103,10 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """commenttttttttttttttttttttttttttttttt"""
-        return self.__dict__
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
