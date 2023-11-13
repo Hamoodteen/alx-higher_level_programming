68e97da64ef1d5987b5e58251fdf8c6d772ae440
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 22:18:49 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ from models.base import Base
 class Rectangle(Base):
     """commenttttttttttttttttttttttttttttttt"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super.__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super.__init__(id)
 
     @property
     def width(self):
