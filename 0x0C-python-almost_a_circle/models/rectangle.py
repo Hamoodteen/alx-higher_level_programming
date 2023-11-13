7c19793f8ecd0dd87f5848daa26e6892a43a51cd
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 22:26:49 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,24 +1,49 @@
 #!/usr/bin/python3
-"""commenttttttttttttttttttttttttttttttt"""
-
+"""Module rectangle.py
+Defines class Rectangle that inherits from Base.
+"""
+import json
 from models.base import Base
 
 
 class Rectangle(Base):
-    """commenttttttttttttttttttttttttttttttt"""
+    """a class Rectangle that inherits from Base.
+    Public instance methods:
+       - area()
+       - display()
+       - to_dictionary()
+       - update()
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialized Rectangle instance.
+
+        Args:
+           - __width: width of the Rectangle
+           - __height: height of the Rectangle.
+           - __x: position
+           - __y: position
+           - id: id
+        """
+
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super.__init__(id)
+        super().__init__(id)
 
     @property
     def width(self):
+        """Retrieves the width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """sets the width of the Rectangle to a value.
+
+        Args:
+           - value: value of the width must be an int.
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -27,10 +52,16 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Retrieves the height of the Rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """sets the height of the Rectangle to a value
+
+        Args:
+           - value: value of the height must be an int.
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,10 +70,16 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Retrieves the x attribute"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """sets the value of position x to a value.
+
+        Args:
+           - value: value of x must be an integer.
+        """
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -51,10 +88,16 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Retrieves the y attribute."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """sets the position y of the Rectangle to a value
+
+        Args:
+           - value: value of y must be an int
+        """
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
