16cc2a4c866c7db65937ff49fff451f92056f4e7
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 23:34:19 +0200

    0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""commenttttttttttttttttttttttttttttttt"""
+"""Module square.py
+class Square that inherits from Rectangle.
+"""
+
 from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """commenttttttttttttttttttttttttttttttt"""
+    """a class Square that inherits from Rectangle.
+    Public instance methods:
+        - area()
+        - display()
+        - to_dictionary()
+        - update()
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializing the Square instances
+
+        Args:
+            - __size: size
+            - __x: position
+            - __y: position
+            - id: id
+        """
+
         self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.size)
+        """Returns a string representation of a Square instance."""
 
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.width = value
-        self.height = value
+        s = "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+        return s
