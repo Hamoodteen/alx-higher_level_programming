ab12e32c901b3fc8584b378c165bda84f780b12d
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 22:23:54 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,40 +15,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """commenttttttttttttttttttttttttttttttt"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """commenttttttttttttttttttttttttttttttt"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """commenttttttttttttttttttttttttttttttt"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """commenttttttttttttttttttttttttttttttt"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
     def x(self):
-        """commenttttttttttttttttttttttttttttttt"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """commenttttttttttttttttttttttttttttttt"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """commenttttttttttttttttttttttttttttttt"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """commenttttttttttttttttttttttttttttttt"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
