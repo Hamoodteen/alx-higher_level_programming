e4a64ff6f522b2d8f52775d4a0729ae01a6c5739
Hamoodteen <ahmed104567@outlook.com>
2023-11-13 22:04:58 +0200

    0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,10 @@
 class Base:
     """commenttttttttttttttttttttttttttttttt"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id:
             self.id = id
         else:
-            self.__nb_objects += 1
-            self.id = self.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
