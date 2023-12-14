4e6e1337e0790ab9e8d1cb52a75fdc5d3f528962
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 20:50:59 +0200

    0

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- sqlsqlsqlsqlsqlsqlsqlsqlsqlsql
-SELECT * FROM `hbtn_0d_usa.cities` WHERE `name` = `California` ORDER BY `cities.id` ASC
+SELECT * FROM `hbtn_0d_usa.cities` WHERE `name` = `California` ORDER BY `cities.id` ASC;
