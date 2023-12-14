ea3065d2fba28a63ce3290301de3da246ca23dd0
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 20:37:30 +0200

    0

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- sqlsqlsqlsqlsqlsqlsqlsqlsqlsql
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (
-	`id` INT DEFAULT 1 NOT NULL,
+	`id` INT AUTO_INCREMENT NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
 );
