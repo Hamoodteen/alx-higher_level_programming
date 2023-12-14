873787ade6f68880eb1825b8b84fbb223ddc73e5
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 20:06:07 +0200

    0

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,3 @@
 -- sqlsqlsqlsqlsqlsqlsqlsqlsqlsql
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
