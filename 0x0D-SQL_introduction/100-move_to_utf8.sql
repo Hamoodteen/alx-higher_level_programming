663637f14e4da7aa9b5cc109514ee5cf727709ab
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 14:18:58 +0200

    0

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Convert specific to UTF-8
-ALTER DATABASE `hbtn_0c_0` SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `first_table` MODIFY COLUMN `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
