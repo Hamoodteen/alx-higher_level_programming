451221fb2ddcb7b5902b8495e2c932b1b6e9452e
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 19:53:04 +0200

    0

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- sqlsqlsqlsqlsqlsqlsqlsqlsqlsql
+-- Create a new user
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
