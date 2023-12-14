009b45f71e0656cd0c4bddc1d6f6285c7ec1027c
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 02:41:34 +0200

    0

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- This is a single-line comment
-INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (98, "Best School");
+INSERT INTO hbtn_0c_0.first_table (`id`, `name`) VALUES (98, "Best School");
