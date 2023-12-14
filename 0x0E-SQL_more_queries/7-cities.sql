c7acf793786b0663cfe0e81d74b655fd5dbcb36c
Hamoodteen <ahmed104567@outlook.com>
2023-12-14 20:42:17 +0200

    0

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- sqlsqlsqlsqlsqlsqlsqlsqlsqlsql
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES cities(id)
+);
