b8d1911c07a71eee95f9df2a78c3a38871c30283
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 17:52:17 +0200

    0

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,4 +3,4 @@
 
 const args = process.argv;
 const cnv = parseInt(args[2], 10);
-console.log(isNaN(cnv) ? 'My number: ' + cnv : 'Not a number');
+console.log(!isNaN(cnv) ? 'My number: ' + cnv : 'Not a number');
