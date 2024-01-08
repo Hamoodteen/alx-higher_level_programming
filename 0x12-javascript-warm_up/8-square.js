9cfaccc901538cde4c32e42cd0cfd52003429862
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 18:27:31 +0200

    0

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,15 +3,18 @@
 
 const args = process.argv;
 const X = 'X';
+let sq = '';
 const cnv = parseInt(args[2], 10);
 if (!isNaN(cnv)) {
   if (cnv > 0) {
     for (let i = 0; i < cnv; i++) {
       for (let j = 0; j < cnv; j++) {
-        console.log(X);
+        sq += X;
       }
+      sq += '\n';
     }
+    console.log(sq.slice(0, -1));
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
