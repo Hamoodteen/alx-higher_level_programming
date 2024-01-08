3c396279595d9160280dfc31f485d786fb73cbc3
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 18:56:57 +0200

    0

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,4 +4,8 @@
 const args = process.argv.slice(2);
 const nums = args.map(Number);
 const mdnums = nums.filter(item => item !== Math.max(...args));
-console.log(Math.max(...mdnums));
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  console.log(Math.max(...mdnums));
+}
