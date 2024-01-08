63f05cfb2543616b4a3f77730ba3e52a780fd212
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 16:34:56 +0200

    0

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 
 const args = process.argv;
 if (args.length > 2) {
-	console.log(process.argv.length() === 3 ? 'Argument found' : 'Arguments found');
+	console.log(process.argv.length === 3 ? 'Argument found' : 'Arguments found');
 } else {
 	console.log('No argument');
 }
