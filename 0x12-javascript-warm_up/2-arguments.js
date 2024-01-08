b083925f7dad6edc7bbdcd7c20b572785149360b
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 16:34:05 +0200

    0

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // commenttttttttttttttttttttttttttttt
 
-import { argv } from 'process';
-if (process.argv.length() > 1) {
-	console.log(process.argv.length() === 2 ? 'Argument found' : 'Arguments found');
-}
-else {
+const args = process.argv;
+if (args.length > 2) {
+	console.log(process.argv.length() === 3 ? 'Argument found' : 'Arguments found');
+} else {
 	console.log('No argument');
 }
