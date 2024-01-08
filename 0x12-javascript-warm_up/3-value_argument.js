4936decff175b90d15032d395c50ef512ab49b68
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 16:53:29 +0200

    0

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// commenttttttttttttttttttttttttttttt
+
+const args = process.argv;
+if (args.length > 2) {
+  console.log(process.argv);
+} else {
+  console.log('No argument');
+}
