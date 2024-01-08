55b8ab434a13627c04b7f2052d4b6332d638ac8e
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 18:06:20 +0200

    0

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,8 @@
 
 const args = process.argv;
 const C = 'C is fun';
-if (Number.isInteger(args[2])) {
+const cnv = parseInt(args[2], 10);
+if (!isNaN(args[2])) {
   if (args[2] > 0) {
     for (let index = 0; index < args[2]; index++) {
       console.log(C);
