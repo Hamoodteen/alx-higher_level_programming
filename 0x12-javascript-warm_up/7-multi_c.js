3309ce9496cbb97097522d15538ba3747f7e8b9b
Hamoodteen <ahmed104567@outlook.com>
2024-01-08 18:04:48 +0200

    0

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// commenttttttttttttttttttttttttttttt
+
+const args = process.argv;
+const C = 'C is fun';
+if (Number.isInteger(args[2])) {
+  if (args[2] > 0) {
+    for (index = 0; index < args[2]; index++) {
+      console.log(C);
+    }
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
