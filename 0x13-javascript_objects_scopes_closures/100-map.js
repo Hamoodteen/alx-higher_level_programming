8481cc37354b3aa71bda9c081ffaf21fd6e8db4d
Hamoodteen <ahmed104567@outlook.com>
2024-01-09 23:06:21 +0200

    0

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 // commenttttttttttttttttttttttttttttt
 
-exports.data = require('./100-data.js').list;
-console.log(exports.data);
-const newlist = exports.data.map((x) => x * exports.data.indexOf(x));
+const data = require('./100-data.js').list;
+console.log(data);
+const newlist = data.map((x) => x * data.indexOf(x));
 console.log(newlist);
