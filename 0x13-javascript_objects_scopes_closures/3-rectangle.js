53d25fedfba7468c546780660fbb0d723352d897
Hamoodteen <ahmed104567@outlook.com>
2024-01-09 21:38:19 +0200

    0

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// commenttttttttttttttttttttttttttttt
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const X = 'X';
+    let sq = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        sq += X;
+      }
+      sq += '\n';
+    }
+    console.log(sq.slice(0, -1));
+  }
+};
