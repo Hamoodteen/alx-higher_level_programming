8fda003ae24a9cdbd14ba568ac7c8b71105c55a0
Hamoodteen <ahmed104567@outlook.com>
2024-01-09 23:31:26 +0200

    0

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,16 +3,6 @@
 
 const fs = require('fs');
 const args = process.argv;
-let first = '';
-let second = '';
-fs.readFile(args[2], 'utf8', (err, data) => {
-  if (err) throw err;
-  first = data;
-});
-fs.readFile(args[3], 'utf8', (err, data) => {
-  if (err) throw err;
-  second = data;
-});
-fs.writeFile(args[4], first + second, 'utf8', (err) => {
-  if (err) throw err;
-});
+const first = fs.readFileSync(args[2], 'utf8');
+const second = fs.readFileSync(args[3], 'utf8');
+fs.writeFileSync(args[4], first + second);
