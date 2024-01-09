bd29b78cf0421ebdc629678987226810bc9b79ad
Hamoodteen <ahmed104567@outlook.com>
2024-01-09 22:22:18 +0200

    0

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,8 +8,8 @@ module.exports = class Square extends require('./5-square') {
     } else {
       const CX = String(c);
       let sq = '';
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
           sq += CX;
         }
         sq += '\n';
