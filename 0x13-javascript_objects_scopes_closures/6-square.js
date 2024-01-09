89982a106e792967b37506b3f536e6b92c6cca6f
Hamoodteen <ahmed104567@outlook.com>
2024-01-09 22:07:51 +0200

    0

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,18 +10,14 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      const CX = String(c);
-      let sq = '';
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
-          sq += CX;
-        }
-        sq += '\n';
+    const CX = (c === undefined ? 'X' : String(c));
+    let sq = '';
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        sq += CX;
       }
-      console.log(sq.slice(0, -1));
+      sq += '\n';
     }
+    console.log(sq.slice(0, -1));
   }
 };
