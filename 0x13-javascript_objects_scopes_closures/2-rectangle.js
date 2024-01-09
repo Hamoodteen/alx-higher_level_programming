3ada03bc1accf0fb8ae7f01c4db3c17036e9611f
Hamoodteen <ahmed104567@outlook.com>
2024-01-09 21:33:41 +0200

    0

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,12 +3,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w > 0) || (h > 0)) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
-    } else {
-      this.width = undefined;
-      this.height = undefined;
     }
   }
 };
