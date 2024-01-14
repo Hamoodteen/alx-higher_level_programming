6b7fadc1f62539e1036d3ffb2211240a6601a578
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 18:23:34 +0200

    0

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttttt"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    new_name = session.query(State).filter(State.name.like('%a%')).first()
+    session.delete(new_name)
+    session.commit()
+    session.close()
