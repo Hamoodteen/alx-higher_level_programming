cd0e2008abfaf28a63154cadd0e33fe57d164dea
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 18:28:04 +0200

    0

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     new_name = session.query(State).filter(State.name.like('%a%')).all()
     for state in new_name:
         session.delete(state)
-        session.commit()
-        session.close()
+    session.commit()
+    session.close()
