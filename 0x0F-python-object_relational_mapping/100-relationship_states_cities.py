3e43f1602383845048c916227fd7c1470d24a1d7
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 20:16:54 +0200

    0

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,7 +3,7 @@
 
 
 import sys
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import Base, City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
