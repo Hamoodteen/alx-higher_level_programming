abde7ef4c1b0a2bb4b0a4e9bbad95d7b2f3bf937
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 18:00:22 +0200

    0

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,7 +2,7 @@
 """commentttttttttttttttttttttttttttttt"""
 
 
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,12 +10,14 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     session = Session()
-    states = session.query(State)\
-        .filter(State.name.like('%a%')).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    state = session.query(State)\
+        .filter(State.name.like('%' + argv[4] + '%')).order_by(State.id).all()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
     session.close()
