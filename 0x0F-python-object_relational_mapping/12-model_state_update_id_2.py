f14ff0f45fde3c8d3847fcd94779d860c3d78601
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 18:20:47 +0200

    0

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""commentttttttttttttttttttttttttttttt"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    Louisiana = State(name='Louisiana')
+    new_name = session.query(State).filter_by(id=2).first()
+    new_name.name = 'New Mexico'
+    session.commit()
+    state = session.query(State).filter(State.name == 'Louisiana').one()
+    print(state.id)
+    session.close()
