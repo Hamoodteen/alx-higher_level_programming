cf96212fdaa6318c11a3734a798f0f9edfe175a0
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 17:56:01 +0200

    0

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,28 +2,20 @@
 """commentttttttttttttttttttttttttttttt"""
 
 
-from sys import argv
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    mysql = {'drivername': 'mysql+mysqldb',
-             'host': 'localhost',
-             'port': '3306',
-             'username': argv[1],
-             'password': argv[2],
-             'database': argv[3],
-             }
-    url = URL(**mysql)
-    engine = create_engine(url, pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     session = Session()
     states = session.query(State)\
-        .filter(State.name.like('%a%')).order_by(State.id)
-    for state in states.all():
+        .filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
         print("{}: {}".format(state.id, state.name))
     session.close()
