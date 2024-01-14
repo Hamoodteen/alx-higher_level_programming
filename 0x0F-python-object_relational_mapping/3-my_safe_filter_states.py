d51019425c4bd77e113548598d8b400683af7d7d
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 15:04:54 +0200

    0

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,10 +15,9 @@ if __name__ == "__main__":
         db=argv[3],
         charset="utf8")
     cur = conn.cursor()
-    qr = "SELECT *\
-                FROM states WHERE name LIKE BINARY ?\
-                ORDER BY id ASC"
-    cur.execute(qr, (argv[4],))
+    cur.execute("SELECT *\
+                FROM states WHERE name = %s\
+                ORDER BY id ASC", (argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
