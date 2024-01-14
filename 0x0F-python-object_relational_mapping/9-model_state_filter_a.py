24903f42b615e101c77d068f7604ded80685b4d8
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 17:54:15 +0200

    0

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,29 +2,28 @@
 """commentttttttttttttttttttttttttttttt"""
 
 
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+if __name__ == "__main__":
+    mysql = {'drivername': 'mysql+mysqldb',
+             'host': 'localhost',
+             'port': '3306',
+             'username': argv[1],
+             'password': argv[2],
+             'database': argv[3],
+             }
+    url = URL(**mysql)
+    engine = create_engine(url, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
-
-    # create a session
     session = Session()
-
-    # extract first state
-    states = session.query(State).filter(State.name.ilike('%a%')) \
-                    .order_by(State.id).all()
-
-    # print states
+    states = session.query(State)\
+        .filter(State.name.like('%a%')).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
-
     session.close()
