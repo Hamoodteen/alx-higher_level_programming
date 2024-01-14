a3bfb4007d3914ca460093f6415c11526c430b8a
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 17:03:11 +0200

    0

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,18 +4,21 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
+
+
+class State(Base):
+    """commenttttttttttttttttttttttttttttttt"""
+    __tablename__ = 'states'
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
 if __name__ == "__main__":
-    Base = declarative_base()
-
-    class State(Base):
-        """commenttttttttttttttttttttttttttttttt"""
-        __tablename__ = 'states'
-        id = Column(Integer,
-                    primary_key=True,
-                    nullable=False,
-                    autoincrement=True)
-        name = Column(String(128), nullable=False)
-
-from model_state import Base, State
-engine = create_engine('sqlite://')
-Base.metadata.create_all(engine)
+    from model_state import Base, State
+    engine = create_engine('sqlite://')
+    Base.metadata.create_all(engine)
