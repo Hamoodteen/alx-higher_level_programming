18cf0c72008cfba4634d54bc045f7128ecb1bd56
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 17:23:42 +0200

    0

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,8 +4,8 @@
 
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
