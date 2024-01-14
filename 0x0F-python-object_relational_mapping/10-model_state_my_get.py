9327fb6c4e62f228329dc63243b246b6738b20fc
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 18:01:47 +0200

    0

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session()
     state = session.query(State)\
-        .filter(State.name.like('%' + argv[4] + '%')).order_by(State.id).all()
+        .filter(State.name.like('%' + argv[4] + '%')).order_by(State.id).first()
     if state:
         print("{}".format(state.id))
     else:
