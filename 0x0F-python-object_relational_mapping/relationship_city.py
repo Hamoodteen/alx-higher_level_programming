8402fc1fdfc7840cae9194fad633ace22329f68c
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 19:57:21 +0200

    0

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """commenttttttttttttttttttttttttttttttt"""
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey('states.id'),
+                      nullable=False)
