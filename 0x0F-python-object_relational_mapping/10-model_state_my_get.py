84816921cc1f8d03825ee13ca5c52de0830364d9
Hamoodteen <ahmed104567@outlook.com>
2024-01-14 18:04:16 +0200

    0

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,7 +2,7 @@
 """commentttttttttttttttttttttttttttttt"""
 
 
-from sys import argv
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,12 +10,12 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     session = Session()
     state = session.query(State)\
-        .filter(State.name.like('%' + argv[4] + '%'))\
+        .filter(State.name.like('%' + sys.argv[4] + '%'))\
         .order_by(State.id).first()
     if state:
         print("{}".format(state.id))
