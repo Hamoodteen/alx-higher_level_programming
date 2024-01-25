4d1c5534d5562d13ef15c4d958a7b5223ef8b6be
Hamoodteen <ahmed104567@outlook.com>
2024-01-25 14:57:24 +0200

    0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #commentttttttttttttttttttttttttttttt
-$res = $(curl -w "%{http_code}" -s -o /dev/null $1)
-if [ "$res" == 200 ]; then
-    curl -s $1
-fi
+if [[ $(curl -w "%{http_code}" -s -o /dev/null $1) == "200" ]]; then curl -s $1; fi
