b09f7e13cef03b18707122c8226771f4083d262a
Hamoodteen <ahmed104567@outlook.com>
2024-01-25 17:16:03 +0200

    0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,6 +10,4 @@ def find_peak(list_of_integers):
 
     if len(list_of_integers) == 0:
         return None
-    if not sorted(list_of_integers):
-        list_of_integers.sort()
-    return max(list_of_integers)
+    return max(sorted(list_of_integers))
