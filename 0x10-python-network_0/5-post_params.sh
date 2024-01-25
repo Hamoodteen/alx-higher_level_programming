b49ca7e84d82ea46e2ad3e1d3189eab5cd8fab30
Hamoodteen <ahmed104567@outlook.com>
2024-01-25 16:15:12 +0200

    0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #commentttttttttttttttttttttttttttttt
-curl -X POST -b "email=test@gmail.com; subject=I will always be here for PLD" -s "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" -s "$1"
