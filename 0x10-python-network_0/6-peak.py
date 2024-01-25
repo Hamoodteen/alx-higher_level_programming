af94b896184b40d12a1d482f17b77cb35c755459
Hamoodteen <ahmed104567@outlook.com>
2024-01-25 17:11:37 +0200

    0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """ Test function find_peak """
 
 
-""" it will be O(n)"""
+""" it will be O(n) or O(log(n))"""
 
 
 def find_peak(list_of_integers):
@@ -10,6 +10,9 @@ def find_peak(list_of_integers):
 
     if len(list_of_integers) == 0:
         return None
+    if not sorted(list_of_integers):
+        list_of_integers.sort()
+        return max(list_of_integers)
     m = 0
     for i in list_of_integers:
         if i > m:
