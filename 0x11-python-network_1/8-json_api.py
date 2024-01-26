2be14020d98d3089d700ce3af074999745b15686
Hamoodteen <ahmed104567@outlook.com>
2024-01-26 16:04:25 +0200

    0

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     from sys import argv
     import requests as rq
     header = '' if len(argv) == 1 else argv[1]
-    req = rq.post("http://0.0.0.0:5000/search_user", {"q": header})
+    data = {"q": header}
+    req = rq.post("http://0.0.0.0:5000/search_user", data=data)
     try:
         rj = req.json()
         if rj == {}:
