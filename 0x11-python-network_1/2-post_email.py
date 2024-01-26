a4fef9be9f9b13d5ab9833010bf4ee82883f5062
Hamoodteen <ahmed104567@outlook.com>
2024-01-26 14:45:41 +0200

    0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""commenttttttttttttttttttttttttttttttt"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    from sys import argv
+    url = argv[1]
+    vars = {'email': argv[2]}
+    email = urllib.parse.urlencode(vars)
+    email = email.encode('utf-8')
+    req = urllib.request.Request(url, email)
+    with urllib.request.urlopen(req) as response:
+        head = response.read()
+        print("Your email is: {}".format(head))
