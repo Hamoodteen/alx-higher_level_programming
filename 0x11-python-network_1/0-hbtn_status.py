39ab17b1e0c081ce7fda35207b39bd6ebc08fefb
Hamoodteen <ahmed104567@outlook.com>
2024-01-26 14:04:05 +0200

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 """commenttttttttttttttttttttttttttttttt"""
 
 
-import urllib
+if __name__ == "__main__":
+	import urllib.request
 
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    """commenttttttttttttttttttttttttttttttt"""
-    html = response.read()
+	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+		html = response.read()
