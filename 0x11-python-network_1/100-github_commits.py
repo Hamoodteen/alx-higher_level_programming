26f91fe0578818152dc8356ba1765b8f1694d805
Hamoodteen <ahmed104567@outlook.com>
2024-01-26 16:23:22 +0200

    0

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""commenttttttttttttttttttttttttttttttt"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests as rq
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = rq.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
