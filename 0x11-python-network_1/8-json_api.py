e2e586f0636824e65b6a95ec3db432c885e6625f
Hamoodteen <ahmed104567@outlook.com>
2024-01-26 15:50:53 +0200

    0

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""commenttttttttttttttttttttttttttttttt"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests as rq
+    headers = {'q': argv[2] if len(argv) > 1 else ""}
+    try:
+        req = rq.post("http://0.0.0.0:5000/search_user", data=headers)
+        req.json()
+        if len(req) == 0:
+            print("No result")
+        else:
+            rhg = req.headers
+            print("[{}] {}".format(rhg.get('id'), rhg.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
