50c3d887a2532888e7b1caa363271671deb71a4d
Hamoodteen <ahmed104567@outlook.com>
2024-02-20 17:28:46 +0200

    0

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,16 @@
 #!/usr/bin/node
-// commenttttttttttttttttttttttttttttt
-
-const req = require('request');
-const args = process.argv;
-const myurl = (args[2]);
-req({ url: myurl, methods: 'GET' }, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const jsonbody = JSON.parse(body);
-    let done = 0;
-    const donelist = [];
-    const donedict = {};
-    for (let i = 1; i < 11; i++) {
-      for (let j = (i - 1) * 20; j < ((i - 1) * 20) + 20; j++) {
-        if (jsonbody[j].completed) {
-          done++;
-        }
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-      donelist.push(done);
-      done = 0;
-    }
-    donelist.forEach((value, index) => {
-      donedict[index + 1] = value;
     });
-    console.log(donedict);
+    console.log(completed);
   }
 });
