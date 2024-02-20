c3c12a1d15736af0318ca0e022f957ed27d97776
Hamoodteen <ahmed104567@outlook.com>
2024-02-20 15:20:01 +0200

    0

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,10 @@
 
 const req = require('request');
 const args = process.argv;
-req(args[2], function (response) {
-  console.log('code: ' + response.statusCode);
+req(args[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
