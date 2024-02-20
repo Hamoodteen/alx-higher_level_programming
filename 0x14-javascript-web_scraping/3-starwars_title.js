3d0b795c1e12fdf2e0a6288421b88bf3d01ace65
Hamoodteen <ahmed104567@outlook.com>
2024-02-20 15:29:17 +0200

    0

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@
 const req = require('request');
 const args = process.argv;
 const myurl = ('https://swapi-api.alx-tools.com/api/films/' + args[2]);
-req({ url: myurl, methods: 'GET' }, function (err, body) {
+req({ url: myurl, methods: 'GET' }, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
