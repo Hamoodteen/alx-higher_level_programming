5b84ebfb7dc90dbccf19f00b3a44d5eadebe1ecd
Hamoodteen <ahmed104567@outlook.com>
2024-02-20 15:06:07 +0200

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@
 const fs = require('fs');
 const args = process.argv;
 const fileName = args[2];
-fs.readFile(fileName, 'utf8', (err, data) => {
+fs.readFile(fileName, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
-    return;
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
